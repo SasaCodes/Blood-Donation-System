4d781953976a2a825faec3cacabec4fc5cb2b2fd
Nishen Sasanka <nishensasanka99@gmail.com>
2025-04-26 00:14:44 +0530

    main function

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blood Donation System</title>
+</head>
+<body>
+    <h1>Blood Donation System</h1>
+</body>
+</html>
